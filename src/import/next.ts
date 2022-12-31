76b3a5ce75fd09834073d58cd07b02dcbc9a2e86
shuu11 <shuu1104main@gmail.com>
2022-12-31 16:11:22 +0900

    change

---
+++ src/import/next.ts
@@ -0,0 +1,9 @@
+import { Inter } from '@next/font/google'
+import Head from 'next/head'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import Script from 'next/script'
+import React from 'react'
+
+export { React, Head, Image, Link, Script, Inter }
